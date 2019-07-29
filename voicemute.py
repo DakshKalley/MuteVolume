87dee89709573c961937e0a4773ac6478d3d1d95
DakshKalley <dakshk117@gmail.com>
2019-07-29 11:08:58 +0530

    Updated program to use google speech-to-text api

--- voicemute.py
+++ voicemute.py
@@ -1,18 +1,17 @@
 import speech_recognition as sr
 import subprocess
 r = sr.Recognizer()
-
 mic = sr.Microphone()
 with mic as source:
     print("Speak into the mic")
     r.adjust_for_ambient_noise(source)
     try:
         audio = r.listen(source)
-        print(r.recognize_sphinx(audio))
-        if (r.recognize_sphinx(audio)) == ('mute'):
+        print(r.recognize_google(audio))
+        if (r.recognize_google(audio)) == ('mute'):
             subprocess.call(['osascript', '-e', 'set volume 0'])
             print("volume muted")
     except sr.UnknownValueError:
-        print("Sphinx could not understand audio")
+        print("Could not understand audio")
     except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
+        print("Voice error; {0}".format(e))
