3e6db5f332edc3599b73b8dc59cb2956d42b34b5
Daksh Kalley <dakshk117@gmail.com>
2022-12-25 13:34:42 -0800

    Updated software to continuously record audio and mute system volume

--- voicemute.py
+++ voicemute.py
@@ -2,16 +2,18 @@ import speech_recognition as sr
 import subprocess
 r = sr.Recognizer()
 mic = sr.Microphone()
-with mic as source:
-    print("Speak into the mic")
-    r.adjust_for_ambient_noise(source)
-    try:
-        audio = r.listen(source)
-        print(r.recognize_google(audio))
-        if (r.recognize_google(audio)) == ('mute'):
-            subprocess.call(['osascript', '-e', 'set volume 0'])
-            print("volume muted")
-    except sr.UnknownValueError:
-        print("Could not understand audio")
-    except sr.RequestError as e:
-        print("Voice error; {0}".format(e))
+
+while True:
+    with mic as source:
+        print("Speak into the mic")
+        r.adjust_for_ambient_noise(source)
+        try:
+            audio = r.listen(source)
+            print(r.recognize_google(audio))
+            if (r.recognize_google(audio)) == ('mute'):
+                subprocess.call(['osascript', '-e', 'set volume with output muted'])
+                print("volume muted")
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+        except sr.RequestError as e:
+            print("Voice error; {0}".format(e))
