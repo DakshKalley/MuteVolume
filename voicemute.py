a75dcb33fb5e3354d56f4e3a18fd9fda8ce9cc97
Daksh Kalley <dakshk117@gmail.com>
2023-11-13 17:03:37 -0600

    using vosk local library and objc instead of osascript for faster commands

--- voicemute.py
+++ voicemute.py
@@ -1,19 +1,48 @@
-import speech_recognition as sr
-import subprocess
-r = sr.Recognizer()
-mic = sr.Microphone()
+from vosk import Model, KaldiRecognizer
+import pyaudio
+import objc
+
+
+model = Model(r"vosk-model-small-en-us-0.15")
+recognizer = KaldiRecognizer(model, 16000)
+
+mic = pyaudio.PyAudio()
+stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream.start_stream()
+
+
+def mute_volume():
+    script = '''
+    tell application "System Events"
+        set volume with output muted
+    end tell
+    '''
+    
+    NSAppleScript = objc.lookUpClass('NSAppleScript')
+    apple_script = NSAppleScript.alloc().initWithSource_(script)
+    apple_script.executeAndReturnError_(None)
+
+def unmute_volume():
+    script = '''
+    tell application "System Events"
+        set volume without output muted
+    end tell
+    '''
+    
+    NSAppleScript = objc.lookUpClass('NSAppleScript')
+    apple_script = NSAppleScript.alloc().initWithSource_(script)
+    apple_script.executeAndReturnError_(None)
+
 
 while True:
-    with mic as source:
-        print("Speak into the mic")
-        r.adjust_for_ambient_noise(source)
-        try:
-            audio = r.listen(source)
-            print(r.recognize_google(audio))
-            if (r.recognize_google(audio)) == ('mute'):
-                subprocess.call(['osascript', '-e', 'set volume with output muted'])
+        data = stream.read(4096, exception_on_overflow = False)
+        if recognizer.AcceptWaveform(data):
+            text = recognizer.Result()
+            cmd = (text[14:-3])
+            print(cmd)
+            if (cmd == 'mute'):
+                mute_volume()
                 print("volume muted")
-        except sr.UnknownValueError:
-            print("Could not understand audio")
-        except sr.RequestError as e:
-            print("Voice error; {0}".format(e))
+            if (cmd == 'speak'):
+                unmute_volume()
+                print("volume unmuted")
